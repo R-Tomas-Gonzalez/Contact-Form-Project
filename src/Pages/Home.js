cdcd18c10083f25f989c437ec88c67770d37e5fc
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2021-06-14 07:51:00 -0500

    added a search bar to the main contact list

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container'
-import UserCards from '../Components/UserCards'
+import Container from '@material-ui/core/Container';
+import UserCards from '../Components/UserCards';
+import TextField from '@material-ui/core/TextField';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,15 +24,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Home = (props) => {
+  const [search, setSearch] = useState("");
   const userProfiles = props.userData;
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
         <h1> Contact List </h1>
+        <TextField onChange={event => {setSearch(event.target.value)}} id="Search" label="Search..." variant="outlined" />
       <Container>
+        <br></br>
         <Grid container spacing={2} justify="center">
-          {userProfiles.map((item, key) => 
+          {userProfiles.filter((val)=> {if (search == "") {
+            return val;
+          } else if(val.firstName.toLowerCase().includes(search.toLowerCase())) {
+            return val;
+          }
+        }).map((item, key) => 
           <Grid key={item.id} item className={classes.itemContainer} xs={12} sm={6} md={3}>
             <UserCards  key={key} {...props} item={item} handleEdits={props.handleEdits} handleDelete={props.handleDelete} />
           </Grid> 
